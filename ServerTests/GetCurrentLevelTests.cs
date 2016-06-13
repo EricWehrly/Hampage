4327f689090f493c0b8f28e58bfec72e221d32b8
Eric <eric@disruptiontheory.com>
2016-06-13 02:14:39 -0400

    Added class for testing getcurrentlevel

---
+++ ServerTests/GetCurrentLevelTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace ServerTests
+{
+    class GetCurrentLevelTests
+    {
+        // make sure the level comes back with valid ... stuff ...
+        // size? is that ... a thing? 
+        // level has basic drawing stuff ... foreground / floor, background ...
+
+            // somewhere, make sure level has buildings
+
+        [Test]
+        public void WillReturnLevelFromFactory()
+        {
+            
+        }
+    }
+}
---
+++ ServerTests/RampageTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ServerTests
+{
+    class RampageTests
+    {
+        [Test]
+        public void willCreateNewLevelOnStartup()
+        {
+            
+        }
+    }
+}
