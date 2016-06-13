f3387616739be74be93700325ede5eb44acfbc5a
Eric <eric@disruptiontheory.com>
2016-06-13 02:09:23 -0400

    Added level classes

---
+++ DisruptoLib/Entities/Level.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SharpKit.JavaScript;
+
+namespace DisruptoLib.Entities
+{
+    [JsType(JsMode.Clr, Filename = "../res/Entities.js")]
+    class Level
+    {
+    }
+}
---
+++ DisruptoLib/Factories/LevelFactory.cs
@@ -0,0 +1,11 @@
+﻿using DisruptoLib.Entities;
+
+namespace DisruptoLib.Factories
+{
+    class LevelFactory
+    {
+        private static Level _currentLevel;
+
+        public static Level CurrentLevel { get { return _currentLevel; } set { _currentLevel = value; } }
+    }
+}
