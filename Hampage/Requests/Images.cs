d5b9662e6d2239ae3ba52de2920ea23e71aeaf82
Eric <eric@disruptiontheory.com>
2016-06-14 19:59:18 -0400

    Fixed images and index
    added nancy route to serve images, and made views/index "content copy
    always" instead of copy never so that the page could be served ...

---
+++ Hampage/Requests/Images.cs
@@ -0,0 +1,19 @@
+﻿using Nancy;
+
+namespace Hampage.Requests
+{
+    public class Images : NancyModule
+    {
+        public Images()
+        {
+            Get["/images/{filename}"] = FetchImage;
+        }
+
+        private Response FetchImage(dynamic parameters)
+        {
+            string filePath = "images/" + parameters.filename;
+
+            return Response.AsFile(filePath, "text/javascript");
+        }
+    }
+}
